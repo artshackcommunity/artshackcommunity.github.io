a356e9ac5dfe100eced1bdf7fea23a623328b6d7
marungo <mrngo@thoughtworks.com>
2025-05-10 09:17:15 -0400

    Points to real mutual aid fund in links section

--- src/app/intro.js
+++ src/app/intro.js
@@ -11,7 +11,7 @@ export default function Intro() {
       <p className={styles.introCopy}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <ul className={styles.introList}>
         <li><a href="/index">Timeline of Events</a></li>
-        <li><a href="/index">Donate to Mutual Aid Fund</a></li>
+        <li><a href="https://opencollective.com/artshack-organizers-mutual-aid">Donate to Mutual Aid Fund</a></li>
         <li><a href="/index">Sign Our Petition</a></li>
         <li><a href="/index">Email Mckendree Key & the Board</a></li>
       </ul>
