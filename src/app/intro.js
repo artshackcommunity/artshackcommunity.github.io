25734098f4dc718b058c37f2667a3b95ed2f36d9
Angelique De Castro <angeliqueajdecastro@gmail.com>
2025-05-11 15:37:07 -0400

    add quotes

--- src/app/intro.js
+++ src/app/intro.js
@@ -1,18 +1,59 @@
+"use client"
+
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import styles from './page.module.css'
+
+const QUOTES = [
+  {
+    text: "Early in life, I learned that if you want something, you better make some noise.",
+    author: "Malcolm X"
+  },
+  {
+    text: "There is no power for change greater than a community discovering what it cares about.",
+    author: "Margaret J. Wheatley"
+  },
+  {
+    text: "The greatness of a community is most accurately measured by the compassionate actions of its members.",
+    author: "Coretta Scott King"
+  }
+];
+
+
+const Quote = ({ author, text}) => {
+
+  return (
+    <p className={styles.quote}>
+      <i>“{text}“</i> <br />
+      <strong>– {author}</strong>
+    </p>
+  )
+}
 
 export default function Intro() {
   const emailSubject = "Save Artshack's Mission!"
   const emailBody = "I'm part of the community and I want you to be restructured to better reflect the wants and needs of the community."
   const emailTemplate = `mailto:recipient@example.com?subject=${emailSubject}&body=${emailBody}`
 
+  const [quote, setQuote] = useState(QUOTES[0]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setQuote((prevQuote) => {
+        const currentIndex = QUOTES.indexOf(prevQuote);
+        const nextIndex = (currentIndex + 1) % QUOTES.length;
+        return QUOTES[nextIndex];
+      });
+    }, 30000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <section className={styles.introContainer}>
       <div className={styles.introCopyContainer}>
         <Image src="/hero.png" alt="Save Artshack's Mission" width="620" height="72" className={styles.hero} />
-        <p className={styles.quote}>
-          “Quote Here about Protecting our community from harm.” -Author
-        </p>
+        <Quote author={quote.author} text={quote.text} />
         <p className={styles.introCopy}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <ul className={styles.introList}>
           <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSc_E2yEf8Vut1CGfx4tQD2mTBAJyVD1e_S5CEcQwbEm-gXIqQ/viewform">Tell Us About Your Artshack Experience</a></li>
