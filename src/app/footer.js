238a81c5f1d096714ded1190403994d2e2c1b98c
Angelique De Castro <angeliqueajdecastro@gmail.com>
2025-05-11 12:36:07 -0400

    fix footer on mobile

--- src/app/footer.js
+++ src/app/footer.js
@@ -5,7 +5,9 @@ import styles from './page.module.css'
 export default function Footer() {
   return (
     <section className={styles.footer}>
-      Questions? Contact us at{" "}<a href="mailto:artshackcommunity@proton.me">artshackcommunity@proton.me</a>
+      <p className={styles.footerContent}>
+        Questions? Contact us at{" "}<a href="mailto:artshackcommunity@proton.me">artshackcommunity@proton.me</a>
+      </p>
     </section>
   )
 }
