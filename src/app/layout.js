3c2258bc079cc0f5f0f50c81db687fe6dbdf76ea
Angelique De Castro <angeliqueajdecastro@gmail.com>
2025-05-11 12:55:21 -0400

    start open graph

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/* <!-- HTML Meta Tags --> */}
+      <title>Artshack Community</title>
+      <meta name="description" content="Protect the Artshack Mission" />
+
+      {/* <!-- Facebook Meta Tags --> */}
+      <meta property="og:url" content="https://artshackcommunity.org" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Artshack Community" />
+      <meta property="og:description" content="Protect the Artshack Mission" />
+      <meta property="og:image" content="" />
+
+      {/* <!-- Twitter Meta Tags --> */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="artshackcommunity.org" />
+      <meta property="twitter:url" content="https://artshackcommunity.org" />
+      <meta name="twitter:title" content="Artshack Community" />
+      <meta name="twitter:description" content="Protect the Artshack Mission" />
+      <meta name="twitter:image" content="" />
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
